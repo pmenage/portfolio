5cc65bcd0b02e91a0936a1a0c4ee8b36617a41f7
pmenage <pauline.menage26@gmail.com>
2017-05-06 21:57:48 +0200

    Changed style of projects

--- components/project.js
+++ components/project.js
@@ -1,47 +1,94 @@
-export default ({ title, image, technologies, description, github }) => (
+import Link from 'next/link'
+
+export default ({ title, image, page, technologies, description, github }) => (
   <div className='main pure-g'>
     <div className='pure-u-1-6'></div>
     <div className='pure-u-2-3'>
       <img src={ image } className='pure-img' />
-      <h2>{ title }</h2>
-      <div className="tech">
-      {
-        technologies.map( (tech, i) => (
-          <span className="technology">{ tech }</span>
-        ))
-      }
+      <div className="gray-on-hover">
+        <div className="view"><span className='view-more'><Link href={ page }><a>View more</a></Link></span></div>
       </div>
-      <div>{ description }</div>
+      <div className="description">
+        <span className="title">{ title }</span>
+        <span className="tech">
+        {
+          technologies.map( (tech, i) => (
+            <span className="technology">{ tech }</span>
+          ))
+        }
+        </span>
+      </div>
     </div>
     <div className='pure-u-1-6'></div>
     <style jsx>{`
       .pure-u-2-3 {
-        background-color: #fff;
-        padding: 3em;
-        margin-bottom: 3em;
-        box-shadow: 0 0 .5em #fff;
+        margin-bottom: 5em;
+        box-shadow: 0 0 .5em #1abc9c;
+        position: relative;
       }
-      h2 {
+      .gray-on-hover {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 1.5em;
+        background-color: none;
+        text-align: center;
+      }
+      .view {
+        position: relative;
+        top: 45%
+      }
+      .view-more {
+        padding: 1.2em 2.3em 1.2em 2.3em;
+        border-radius: .5em;
+        background-color: #1abc9c;
+        opacity: 0;
+      }
+      .view-more > a {
+        text-decoration: none;
+        font-family: Raleway;
+        color: #2c3e50;
+        font-size: 18px;
+        font-weight: 700;
+      }
+      .gray-on-hover:hover {
+        background-color: rgba(52, 73, 94, 0.7);
+        -webkit-transition: .5s ease-in-out;
+      }
+      .gray-on-hover:hover .view-more {
+        opacity: 1;
+        -webkit-transition: 1s ease-in-out;
+      }
+      .description {
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        padding: 1.5em;
+        background-color: #bdc3c7;
+      }
+      .title {
         font-family: Raleway;
         font-size: 30px;
-        font-weight: 200;
-        color: #16a085;
+        font-weight: 700;
+        color: #2c3e50;
+        
       }
       div {
         color: #34495e;
       }
-      .pure-img {
-        float: left;
-        margin-right: 3em;
-      }
       .tech {
-        margin-bottom: 1em;
+        margin-top: .5em;
+        float: right;
+        margin-bottom: 0;
       }
       .technology {
-        border: .1em solid #16a085;
-        padding: .3em;
+        margin-bottom: 0;
+        padding: .4em;
         border-radius: .5em;
         margin-right: 1em;
+        background-color: #fff;
       }
     `}</style>
   </div>
--- pages/projects.js
+++ pages/projects.js
@@ -13,12 +13,13 @@ export default class extends React.Component {
   render () {
     return (
       <Layout title="Work">
-        <H1 title="― Some of my projects ―"></H1>
+        <H1 title="Some of my projects"></H1>
         {
           this.props.projects.map( (project, i) => (
             <Project
               title={ project.name }
               image={ project.image }
+              page={ project.page }
               technologies={ project.technologies }
               description={ project.description }
               github={ project.github }>
