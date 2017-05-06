b8d982f438749361bb5f85775217c3dce83f0a5e
pmenage <pauline.menage26@gmail.com>
2017-05-06 21:57:57 +0200

    Added data

--- data/projects.js
+++ data/projects.js
@@ -1,28 +1,32 @@
 const projects = [
   {
     "name": "Website for AEI",
-    "image": "http://loremflickr.com/320/300",
+    "image": "/static/AEI.png",
+    "page": "/projects/aei",
     "technologies": ["WordPress", "Bootstrap"],
     "description": "This website was created for the student organization of which I was president. The goal was to create a simple website, easily maintainable by all collaborators. I created a custom theme, to make the website unique.",
     "github": "https://github/pmenage/aei-website"
   },
   {
     "name": "Pulseo",
-    "image": "http://loremflickr.com/320/300",
+    "image": "/static/Pulseo.png",
+    "page": "/projects/pulseo",
     "technologies": ["Java EE", "JSF", "JDBC", "Primefaces", "MySQL"],
     "description": "With a team, we created an application to help an incubator manage its startups. The application lets the people using it get information on each startup and get statistics on the incubator itself. The goal was to help the incubator replace its Excel sheets by an app.",
     "github": "https://github/pmenage/aei-website"
   },
   {
     "name": "Bumple game",
-    "image": "http://loremflickr.com/320/300",
+    "image": "/static/AEI.png",
+    "page": "/projects/bumple",
     "technologies": ["Java", "Processing"],
     "description": "This game was developed with Java and the Processing library on my personal time. The goal is to make a character jump vertically from obstacle to obstacle, without being caught by the shark.",
     "github": "https://github/pmenage/bumple-game"
   },
   {
     "name": "Purchase records application",
-    "image": "http://loremflickr.com/320/300",
+    "image": "/static/Purchases.png",
+    "page": "/projects/purchase-records",
     "technologies": ["Node.js", "MySQL", "Bootstrap"],
     "description": "The goal of this app is to manage purchases between roommates.",
     "github": "https://github/pmenage/purchase-records-app"
