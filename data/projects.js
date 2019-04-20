07688c58794a85f8c9d707ce19246210a0ad1be1
Pauline Ménage <pauline.menage26@gmail.com>
2019-04-20 18:48:23 +0200

    Update projects

--- data/projects.js
+++ data/projects.js
@@ -1,50 +1,55 @@
 const projects = [
   {
-    "name": "Commercial website",
-    "image": "/static/AEI.png",
-    "slug": "aei",
-    "technologies": ["WordPress /", "Bootstrap /", "PHP"],
-    "description": "This website was created for the Junior-Enterprise of which I was president. The goal was to create a simple website, easily maintainable by all collaborators. I created a custom theme, to make the website unique.",
-    "github": "https://github.com/pmenage/aei-website"
+    name: "Uppiz",
+    image: "/static/Uppiz.png",
+    slug: "uppiz",
+    technologies: ["Angular /", "Node.js /", "MySQL"],
+    description:
+      "Uppiz organizes trips for people with disabilities. This website lets people sign up as users, place owners or companions to help people organize their trip effectively."
   },
   {
-    "name": "Bumple game",
-    "image": "/static/Bumple.png",
-    "slug": "bumple",
-    "technologies": ["Java /", "Maven /", "Processing"],
-    "description": "This game was developed with Java and the Processing library on my personal time. The goal is to make a character jump vertically from obstacle to obstacle, without being caught by the shark.",
-    "github": "https://github.com/pmenage/bumple-game"
+    name: "HoloMake",
+    image: "/static/Holomake.png",
+    slug: "holomake",
+    technologies: ["React /", "Firebase /", "Google App Engine"],
+    description:
+      "Creation of the website and authentication service for HoloMake, an augmented reality startup."
   },
   {
-    "name": "Bysykkel Bot",
-    "image": "/static/Bysykkel.png",
-    "slug": "bysykkel",
-    "technologies": ["Go /", "Docker /", "Kubernetes"],
-    "description": "This is a multilingual Telegram chatbot which tells you if there are bikes or locks near you for the city bicycles. It is deployed on Google Container Engine.",
-    "github": "https://github.com/pmenage/bysykkel-bot"
+    name: "Commercial website",
+    image: "/static/AEI.png",
+    slug: "aei",
+    technologies: ["WordPress /", "Bootstrap /", "PHP"],
+    description:
+      "This website was created for the Junior-Enterprise of which I was president. The goal was to create a simple website, easily maintainable by all collaborators. I created a custom theme, to make the website unique.",
+    github: "https://github.com/pmenage/aei-website"
   },
   {
-    "name": "Volunteering app",
-    "image": "/static/Volunteer.png",
-    "slug": "volunteer",
-    "technologies": ["AngularJS /", "Bootstrap /", "jQuery"],
-    "description": "This is an app implemented in AngularJS and Bootstrap. Only the front-end is implemented for now, and special care was taken for the design.",
-    "github": "https://github.com/pmenage/volunteers"
+    name: "Bumple game",
+    image: "/static/Bumple.png",
+    slug: "bumple",
+    technologies: ["Java /", "Maven /", "Processing"],
+    description:
+      "This game was developed with Java and the Processing library on my personal time. The goal is to make a character jump vertically from obstacle to obstacle, without being caught by the shark.",
+    github: "https://github.com/pmenage/bumple-game"
   },
   {
-    "name": "Pulseo",
-    "image": "/static/Pulseo.png",
-    "slug": "pulseo",
-    "technologies": ["Java EE /", "JSF /", "JDBC /", "MySQL"],
-    "description": "With a team, we created an application to help an incubator manage its startups. The application helps manage finances, meetings, and information on the startups. The goal was to help the incubator replace its Excel sheets by an app."
+    name: "Bysykkel Bot",
+    image: "/static/Bysykkel.png",
+    slug: "bysykkel",
+    technologies: ["Go /", "Docker /", "Kubernetes"],
+    description:
+      "This is a multilingual Telegram chatbot which tells you if there are bikes or locks near you for the city bicycles. It is deployed on Google Container Engine.",
+    github: "https://github.com/pmenage/bysykkel-bot"
   },
   {
-    "name": "Budgeting assistant",
-    "image": "/static/Purchases.png",
-    "slug": "purchase-records",
-    "technologies": ["Node.js /", "MySQL /", "Bootstrap"],
-    "description": "The goal of this app is to manage purchases between roommates. It is easy to access the different information on the purchases in a shared home. This project was focused on a very clean and efficient database schema.",
-    "github": "https://github.com/pmenage/purchase-records-app"
+    name: "Volunteering app",
+    image: "/static/Volunteer.png",
+    slug: "volunteer",
+    technologies: ["AngularJS /", "Bootstrap /", "jQuery"],
+    description:
+      "This is an app implemented in AngularJS and Bootstrap. Only the front-end is implemented for now, and special care was taken for the design.",
+    github: "https://github.com/pmenage/volunteers"
   }
 ];
 
