af6c924e8353380c5895eee122a8dfa318bcb306
Pauline Ménage <pauline.menage26@gmail.com>
2019-04-21 12:58:14 +0200

    Reorder columns

--- pages/index.js
+++ pages/index.js
@@ -31,49 +31,122 @@ export default () => (
         </div>
 
         <div id='page_wrap'>
+            <div id='left'>
+                <div>
+                    <h2>Technologies</h2>
+                    <div className='info'><div className='leftinfo'>Web <small>(front-end)</small></div><div>React, Angular, Vue, Bootstrap</div></div>
+                    <div className='info'><div className='leftinfo'>Web <small>(back-end)</small></div><div >Node.js, PHP, MongoBD, MySQL, PostgreSQL</div></div>
+                    <div className='info'><div className='leftinfo'>Mobile</div><div>React Native, Android</div>
+                    <div className='info'><div className='leftinfo'>Cloud / Devops</div><div >Google Cloud Platform, Amazon Web Services, Docker, Kubernetes</div></div>
+                    <div className='info'><div className='leftinfo'>System</div><div>C, Go, C#, .NET Core, Java, Python</div></div>
+                </div>
 
-          <div id='right'>
-            <div>
-                <h2>Experience</h2>
+                <div>
+                    <h2>Volunteering</h2>
+                    <div className='exp'>
+                        <div className='experience'>President of a <a className='atitle' target='_blank' href='https://junior-aei.com/'>Junior-Enterprise</a></div>
+                        <div>Bordeaux, France</div>
+                        <div className='date'>April 2016 – April 2017</div>
+                        <ul>
+                            <li>Managed 23 collaborators</li>
+                            <li>Established middle- and long-term strategies</li>
+                            <li>Negotiated partnerships</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div>
+                <h2>Projects</h2>
 
                 <div className='exp'>
-                    <div className='experience'>Software Engineer (intern then freelance)</div>
-                    <div><a target='_blank' href='https://www.thermofisher.com'>Thermo Fisher Scientific</a> - Bordeaux, France <span className='date'>// February – August 2018</span></div>
-                    C#, .NET Core, SignalR, TypeScript, Angular
+                    <div className='experience'>Followed a Machine Learning MOOC</div>
+                    <div>Stanford University - 2017</div>
+                </div>
+
+                <div className='exp'>
+                    <div className='experience'>Open-source projects</div>
                     <ul>
-                        <li>Development of realtime comments and annotations</li>
-                        <li>Research about collaborative text editing using CRDT and OT</li>
+                        <li>Video game (Java, Maven, Processing)</li>
+                        <li>Chatbot to get city bikes (Golang, Kubernetes)</li>
+                    </ul>
+                </div>
+                </div>
+
+                <div>
+                    <h2>Languages</h2>
+                    <div className='info'><span className='leftlang'>French</span>- Mother tongue</div>
+                    <div className='info'><span className='leftlang'>English</span>- Bilingual <small>(5 years in the USA)</small></div>
+                    <div className='info'><span className='leftlang'>Spanish</span>- Good working knowledge</div>
+                </div>
+
+                <div>
+                    <h2>Interests</h2>
+                    <ul>
+                        <li>Horseback riding <small>Galop&nbsp;7, competitions</small></li>
+                        <li>Piano <small>7 years</small></li>
+                        <li>Debate club</li>
                     </ul>
                 </div>
 
-                <div className='exp'>
-                    <div className='experience'>Software Engineering Intern</div>
-                    <div><a target='_blank' href='https://unacast.com'>Unacast</a> - Oslo, Norway <span className='date'>// June – September 2017</span></div>
-                    GCP, AWS, Kubernetes, Docker, Golang
-                    <ul>
-                        <li>Creation of a pipeline to transfer files from S3 to BigQuery</li>
-                        <li>Automation of tasks with a Slack chatbot</li>
-                        <li>Development of microservices from specification to production</li>
-                    </ul>
+                <div id='contact'>
+                    <h2>Contact</h2>
+                    <div className='titlecontact'>
+                        66 Rue Denfert Rochereau<br />
+                        92100 Boulogne-Billancourt<br />
+                        France
+                    </div>
+
+                    <div className='titlecontact'>
+                        paulinemenage.com<br />
+                        <a href="https://github.com/pmenage" target="_blank">github.com/pmenage</a><br />
+                        +33 (0)6 45 47 42 43<br />
+                        pauline.menage26@gmail.com
+                    </div>
+                </div>
+            </div>
+            </div>
+            <div id='right'>
+                <div>
+                    <h2>Experience</h2>
+
+                    <div className='exp'>
+                        <div className='experience'>Software Engineer (intern then freelance)</div>
+                        <div><a target='_blank' href='https://www.thermofisher.com'>Thermo Fisher Scientific</a> - Bordeaux, France <span className='date'>// February – August 2018</span></div>
+                        C#, .NET Core, SignalR, TypeScript, Angular
+                        <ul>
+                            <li>Development of realtime comments and annotations</li>
+                            <li>Research about collaborative text editing using CRDT and OT</li>
+                        </ul>
+                    </div>
+
+                    <div className='exp'>
+                        <div className='experience'>Software Engineering Intern</div>
+                        <div><a target='_blank' href='https://unacast.com'>Unacast</a> - Oslo, Norway <span className='date'>// June – September 2017</span></div>
+                        GCP, AWS, Kubernetes, Docker, Golang
+                        <ul>
+                            <li>Creation of a pipeline to transfer files from S3 to BigQuery</li>
+                            <li>Automation of tasks with a Slack chatbot</li>
+                            <li>Development of microservices from specification to production</li>
+                        </ul>
+                    </div>
+
+                    <div className='exp'>
+                        <div className='experience'>3<sup>rd</sup> place in the Atos IT Challenge 2017</div>
+                        <div>Created a ticket selling mobile <a target='_blank' href='https://www.atositchallenge.net/edition-2017/'>app</a> based on the Ethereum blockchain</div>
+                    </div>
+
+                    <div className='exp'>
+                        <div className='experience'>Business and Software Engineering Intern</div>
+                        <div><a target='_blank' href='http://captiz.com/'>Captiz</a> - Bordeaux, France <span className='date'>// July – August 2016</span></div>
+                        <ul>
+                            <li>Developed a Node.js software module</li>
+                            <li>Negotiated contracts (11k€) and created marketing material</li>
+                        </ul>
+                    </div>
+
                 </div>
 
-                <div className='exp'>
-                    <div className='experience'>3<sup>rd</sup> place in the Atos IT Challenge 2017</div>
-                    <div>Created a ticket selling mobile <a target='_blank' href='https://www.atositchallenge.net/edition-2017/'>app</a> based on the Ethereum blockchain</div>
-                </div>
-
-                <div className='exp'>
-                    <div className='experience'>Business and Software Engineering Intern</div>
-                    <div><a target='_blank' href='http://captiz.com/'>Captiz</a> - Bordeaux, France <span className='date'>// July – August 2016</span></div>
-                    <ul>
-                        <li>Developed a Node.js software module</li>
-                        <li>Negotiated contracts (11k€) and created marketing material</li>
-                    </ul>
-                </div>
-
-              </div>
-
-            <div>
+                <div>
                     <h2>Education</h2>
                     <div>
                         <div className='formation'>
@@ -91,86 +164,9 @@ export default () => (
                             <div>Equivalent to 'A' levels in math, physics, and science</div>
                             <div><a target='_blank' href='http://www.ecolejeanninemanuel.org/en/public.html'>Ecole Jeannine Manuel</a> (international school), Paris, France <span className='date'>// 2013</span></div>
                         </div>
-
                     </div>
                 </div>
-
-          </div>
-
-          <div id='left'>
-            <div>
-                <h2>Technologies</h2>
-                <div className='info'><div className='leftinfo'>Web <small>(front-end)</small></div><div>React, Angular, Vue, Bootstrap</div></div>
-                <div className='info'><div className='leftinfo'>Web <small>(back-end)</small></div><div >Node.js, PHP, MongoBD, MySQL, PostgreSQL</div></div>
-                <div className='info'><div className='leftinfo'>Mobile</div><div>React Native, Android</div>
-                <div className='info'><div className='leftinfo'>Cloud / Devops</div><div >Google Cloud Platform, Amazon Web Services, Docker, Kubernetes</div></div>
-                <div className='info'><div className='leftinfo'>System</div><div>C, Go, C#, .NET Core, Java, Python</div></div>
             </div>
-
-            <div>
-                <h2>Volunteering</h2>
-                <div className='exp'>
-                    <div className='experience'>President of a <a className='atitle' target='_blank' href='https://junior-aei.com/'>Junior-Enterprise</a></div>
-                    <div>Bordeaux, France</div>
-                    <div className='date'>April 2016 – April 2017</div>
-                    <ul>
-                        <li>Managed 23 collaborators</li>
-                        <li>Established middle- and long-term strategies</li>
-                        <li>Negotiated partnerships</li>
-                    </ul>
-                </div>
-            </div>
-
-            <div>
-              <h2>Projects</h2>
-
-              <div className='exp'>
-                  <div className='experience'>Followed a Machine Learning MOOC</div>
-                  <div>Stanford University - 2017</div>
-              </div>
-
-              <div className='exp'>
-                  <div className='experience'>Open-source projects</div>
-                  <ul>
-                      <li>Video game (Java, Maven, Processing)</li>
-                      <li>Chatbot to get city bikes (Golang, Kubernetes)</li>
-                  </ul>
-              </div>
-            </div>
-
-            <div>
-                <h2>Languages</h2>
-                    <div className='info'><span className='leftlang'>French</span>- Mother tongue</div>
-                    <div className='info'><span className='leftlang'>English</span>- Bilingual <small>(5 years in the USA)</small></div>
-                    <div className='info'><span className='leftlang'>Spanish</span>- Good working knowledge</div>
-            </div>
-
-            <div>
-                <h2>Interests</h2>
-                        <ul>
-                            <li>Horseback riding <small>Galop&nbsp;7, competitions</small></li>
-                            <li>Piano <small>7 years</small></li>
-                            <li>Debate club</li>
-                        </ul>
-            </div>
-
-             <div id='contact'>
-                <h2>Contact</h2>
-                <div className='titlecontact'>
-                    66 Rue Denfert Rochereau<br />
-                    92100 Boulogne-Billancourt<br />
-                    France
-                </div>
-
-                <div className='titlecontact'>
-                    paulinemenage.com<br />
-                    <a href="https://github.com/pmenage" target="_blank">github.com/pmenage</a><br />
-                    +33 (0)6 45 47 42 43<br />
-                    pauline.menage26@gmail.com
-                </div>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
       <style jsx>{`
