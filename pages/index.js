4e4187933b4b388c0aedf64904f7c32d38255fa5
Pauline Ménage <pauline.menage26@gmail.com>
2019-04-21 13:45:34 +0200

    Update with new experiences

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default () => (
           <h1>Pauline Ménage</h1>
 
           <div className='titletop'>
-              <div>Software Engineer</div>
+              <div>Freelance Software Engineer</div>
               <div>Master's Degree</div>
           </div>
 
@@ -34,19 +34,21 @@ export default () => (
             <div id='left'>
                 <div>
                     <h2>Technologies</h2>
-                    <div className='info'><div className='leftinfo'>Web <small>(front-end)</small></div><div>React, Angular, Vue, Bootstrap</div></div>
-                    <div className='info'><div className='leftinfo'>Web <small>(back-end)</small></div><div >Node.js, PHP, MongoBD, MySQL, PostgreSQL</div></div>
-                    <div className='info'><div className='leftinfo'>Mobile</div><div>React Native, Android</div>
-                    <div className='info'><div className='leftinfo'>Cloud / Devops</div><div >Google Cloud Platform, Amazon Web Services, Docker, Kubernetes</div></div>
-                    <div className='info'><div className='leftinfo'>System</div><div>C, Go, C#, .NET Core, Java, Python</div></div>
+                    <div><h3>Web <small>(front-end)</small></h3><div>React, Angular, Vue, Bootstrap</div></div>
+                    <div><h3>Web <small>(back-end)</small></h3><div >Node.js, PHP, MongoBD, MySQL, PostgreSQL</div></div>
+                    <div><h3>Mobile</h3><div>React Native, Android</div>
+                    <div><h3>Cloud / Devops</h3><div >Google Cloud Platform, Amazon Web Services, Docker, Kubernetes</div></div>
+                    <div><h3>System</h3><div>C, Go, C#, .NET Core, Java, Python</div></div>
                 </div>
 
                 <div>
                     <h2>Volunteering</h2>
-                    <div className='exp'>
-                        <div className='experience'>President of a <a className='atitle' target='_blank' href='https://junior-aei.com/'>Junior-Enterprise</a></div>
-                        <div>Bordeaux, France</div>
-                        <div className='date'>April 2016 – April 2017</div>
+                    <div className='experience'>
+                        <h3>President of a Junior-Enterprise - AEI</h3>
+                        <div className='row'>
+                            <div>Bordeaux, France</div>
+                            <div className='date'>// April 2016 – April 2017</div>
+                        </div>
                         <ul>
                             <li>Managed 23 collaborators</li>
                             <li>Established middle- and long-term strategies</li>
@@ -58,13 +60,18 @@ export default () => (
                 <div>
                 <h2>Projects</h2>
 
-                <div className='exp'>
-                    <div className='experience'>Followed a Machine Learning MOOC</div>
+                <div className='experience'>
+                    <h3>3<sup>rd</sup> place in the Atos IT Challenge 2017</h3>
+                    <div>Created a ticket selling mobile <a target='_blank' href='https://www.atositchallenge.net/edition-2017/'>app</a> based on the Ethereum blockchain</div>
+                </div>
+
+                <div className='experience'>
+                    <h3>Followed a Machine Learning MOOC</h3>
                     <div>Stanford University - 2017</div>
                 </div>
 
-                <div className='exp'>
-                    <div className='experience'>Open-source projects</div>
+                <div className='experience'>
+                    <h3>Open-source projects</h3>
                     <ul>
                         <li>Video game (Java, Maven, Processing)</li>
                         <li>Chatbot to get city bikes (Golang, Kubernetes)</li>
@@ -74,9 +81,9 @@ export default () => (
 
                 <div>
                     <h2>Languages</h2>
-                    <div className='info'><span className='leftlang'>French</span>- Mother tongue</div>
-                    <div className='info'><span className='leftlang'>English</span>- Bilingual <small>(5 years in the USA)</small></div>
-                    <div className='info'><span className='leftlang'>Spanish</span>- Good working knowledge</div>
+                    <div className='language row'><h3>French</h3><div>- Mother tongue</div></div>
+                    <div className='language row'><h3>English</h3><div>- Bilingual <small>(5 years in the USA)</small></div></div>
+                    <div className='language row'><h3>Spanish</h3><div>- Good working knowledge</div></div>
                 </div>
 
                 <div>
@@ -109,35 +116,52 @@ export default () => (
                 <div>
                     <h2>Experience</h2>
 
-                    <div className='exp'>
-                        <div className='experience'>Software Engineer (intern then freelance)</div>
-                        <div><a target='_blank' href='https://www.thermofisher.com'>Thermo Fisher Scientific</a> - Bordeaux, France <span className='date'>// February – August 2018</span></div>
-                        C#, .NET Core, SignalR, TypeScript, Angular
+                    <div className='experience'>
+                        <h3>Freelance Software Engineer</h3>
+                        <div>January 2019 - present</div>
                         <ul>
-                            <li>Development of realtime comments and annotations</li>
-                            <li>Research about collaborative text editing using CRDT and OT</li>
+                            <li>Wynn <span className='date'>// Vue, GraphQL, Hasura</span></li>
+                            <li>Chaville <span className='date'>// WordPress Rest API, React, Redux</span></li>
+                            <li>Uppiz <span className='date'>// Angular, Node.js</span></li>
+                            <li>HoloMake <span className='date'>// React, Firebase, Google App Engine</span></li>
                         </ul>
                     </div>
 
-                    <div className='exp'>
-                        <div className='experience'>Software Engineering Intern</div>
-                        <div><a target='_blank' href='https://unacast.com'>Unacast</a> - Oslo, Norway <span className='date'>// June – September 2017</span></div>
+                    <div className='experience'>
+                        <h3>Mentor and course creator - OpenClassrooms</h3>
+                        <div>January 2019 - present</div>
+                        <ul>
+                            <li>Course creator <span className='date'>// Visual Studio and unit testing</span></li>
+                            <li>Mentor <span className='date'>// HTML, CSS, PHP, JavaScript, MySQL</span></li>
+                            <li>Technical proofreader <span className='date'>// ASP.NET Core</span></li>
+                        </ul>
+                    </div>
+
+                    <div className='experience'>
+                        <h3>Software Engineer - Thermo Fisher Scientific</h3>
+                        <div>Bordeaux, France <span className='date'>// February – December 2018</span></div>
+                        <div>Angular, .NET Core, PostgreSQL, Docker, Kubernetes</div>
+                        <ul>
+                            <li>Developed realtime comments and annotations</li>
+                            <li>Created a file sharing functionality</li>
+                            <li>Deployed the website with Docker and Kubernetes</li>
+                        </ul>
+                    </div>
+
+                    <div className='experience'>
+                        <h3>Software Engineer - Unacast</h3>
+                        <div>Oslo, Norway <span className='date'>// June – September 2017</span></div>
                         GCP, AWS, Kubernetes, Docker, Golang
                         <ul>
-                            <li>Creation of a pipeline to transfer files from S3 to BigQuery</li>
-                            <li>Automation of tasks with a Slack chatbot</li>
-                            <li>Development of microservices from specification to production</li>
+                            <li>Created a pipeline to transfer files from S3 to BigQuery</li>
+                            <li>Automated tasks with a Slack chatbot</li>
+                            <li>Developed microservices from specification to production</li>
                         </ul>
                     </div>
 
-                    <div className='exp'>
-                        <div className='experience'>3<sup>rd</sup> place in the Atos IT Challenge 2017</div>
-                        <div>Created a ticket selling mobile <a target='_blank' href='https://www.atositchallenge.net/edition-2017/'>app</a> based on the Ethereum blockchain</div>
-                    </div>
-
-                    <div className='exp'>
-                        <div className='experience'>Business and Software Engineering Intern</div>
-                        <div><a target='_blank' href='http://captiz.com/'>Captiz</a> - Bordeaux, France <span className='date'>// July – August 2016</span></div>
+                    <div className='experience'>
+                        <h3>Software and business intern - Captiz</h3>
+                        <div>Bordeaux, France <span className='date'>// July – August 2016</span></div>
                         <ul>
                             <li>Developed a Node.js software module</li>
                             <li>Negotiated contracts (11k€) and created marketing material</li>
@@ -149,18 +173,18 @@ export default () => (
                 <div>
                     <h2>Education</h2>
                     <div>
-                        <div className='formation'>
-                            <div className='ecole'>Master's Degree in Software Engineering</div>
+                        <div>
+                            <h3>Master's Degree in Software Engineering</h3>
                             <div>Bordeaux Graduate School of Engineering</div>
                             <div><a target='_blank' href='https://enseirb-matmeca.bordeaux-inp.fr/en'>ENSEIRB-MATMECA</a>, Bordeaux, France <span className='date'>// 2015 – 2018</span></div>
                         </div>
-                        <div className='formation'>
-                            <div className='ecole'>Intensive Math and Physics Course</div>
+                        <div>
+                            <h3>Intensive Math and Physics Course</h3>
                             <div>Preparation for the selective entrance into French graduate schools</div>
                             <div><a target='_blank' href='http://www.lycee-saintlouis.fr/'>Lycée Saint-Louis</a>, Paris, France <span className='date'>// 2013 – 2015</span></div>
                         </div>
-                        <div className='formation'>
-                            <div className='ecole'>International Option Baccalaureate (OIB)</div>
+                        <div>
+                            <h3>International Option Baccalaureate (OIB)</h3>
                             <div>Equivalent to 'A' levels in math, physics, and science</div>
                             <div><a target='_blank' href='http://www.ecolejeanninemanuel.org/en/public.html'>Ecole Jeannine Manuel</a> (international school), Paris, France <span className='date'>// 2013</span></div>
                         </div>
@@ -177,6 +201,10 @@ export default () => (
             padding: 1em;
             -webkit-print-color-adjust: exact;
             line-height: 1.7;
+        }
+
+        .row {
+            display: flex;
         }
 
         h1 {
@@ -196,6 +224,14 @@ export default () => (
             color: #c0392b;
             letter-spacing: .05cm;
             margin-bottom: 5px;
+        }
+
+        h3 {
+            font-family: 'Lato';
+            color: #2980b9;
+            font-size: 110%;
+            font-weight: 500;
+            margin: 5px 0;
         }
 
         #title {
@@ -238,49 +274,25 @@ export default () => (
             flex-direction: column-reverse;
         }
 
-        .formation {
-            margin-bottom: 10px;
-        }
-
         .date {
             font-weight: 200;
             font-size: 11pt;
         }
 
-        .ecole, .experience {
-            font-weight: 700;
-        }
-
-        .exp {
-            margin-bottom: 10px;
-        }
-
-        .exp ul {
+        .experience ul {
             margin-top: 5px;
-        }
-
-        span {
-            margin-right: 4px;
         }
 
         li {
             list-style-type: square;
         }
 
-        .info {
-            padding-right: 20px;
+        .language {
+            align-items: center;
         }
 
-        .leftlang {
-            font-weight: bold;
-        }
-
-        .leftinfo, .leftlang, .ecole, .experience {
-            font-family: 'Lato';
-            color: #2980b9;
-            font-size: 110%;
-            font-weight: 500;
-            margin-bottom: 5px;
+        .language div {
+            margin: 0 0 -5px 4px;
         }
 
         @page {
@@ -288,22 +300,9 @@ export default () => (
             margin: 1cm;
         }
 
-        @media screen {
-            a, a:visited, a:hover {
-                color: #6E8A8D !important;
-                text-decoration: none;
-                border-bottom: 1pt dotted #6E8A8D;
-            }
-        }
-
-        @media print {
-            a, a:visited, a:hover {
-                text-decoration: none;
-                color: #2c3e50;
-            }
-            .atitle {
-                color: #c0392b;
-            }
+        a, a:visited, a:hover {
+            text-decoration: none;
+            color: #2c3e50;
         }
 
         @media print, (min-width: 900px) {
