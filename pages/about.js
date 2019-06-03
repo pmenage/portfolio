d444078e37d5a467a260a9acad8981719d58407f
Pauline Ménage <pauline.menage26@gmail.com>
2019-06-03 21:38:41 +0200

    Add alt to image in about

--- pages/about.js
+++ pages/about.js
@@ -1,32 +1,33 @@
 import Layout from '../components/layout'
 
 export default () => (
-	<Layout title="About">
-		<h1>About</h1>
+  <Layout title="About">
+    <h1>About</h1>
 
-		<div className='pure-g'>
+    <div className='pure-g'>
 
-			<div className='pure-u-sm-1 pure-u-md-1-2'>
-				<div className='info'>
-					<img src='../static/Pauline.jpg' className='pure-img image' />
-					<div className='links'>
-						<a href='https://github.com/pmenage' rel="noopener" target="_blank">Github profile</a><br />
-						<a href='https://www.linkedin.com/in/pauline-m%C3%A9nage-030479109/' rel="noopener" target="_blank">LinkedIn profile</a><br />
-						<a href='https://medium.com/@pauline.menage26' rel="noopener" target="_blank">Medium blog posts</a>
-					</div>
-				</div>
-			</div>
+      <div className='pure-u-sm-1 pure-u-md-1-2'>
+        <div className='info'>
+          <img src='../static/Pauline.jpg' alt="Pauline Ménage" className='pure-img image' />
 
-			<div className='pure-u-sm-1 pure-u-md-1-2'>
-				<div className='about'>
-					<h2>About me</h2>
-					<p>Hello, I am Pauline, a software engineering student completing my fifth year at Bordeaux Graduate School of Engineering, in Computer Science. I am passionate about computer science, and I spend most of my free time coding or reading about new technologies.</p>
-					<p>As president of a Junior-Enterprise in 2016-2017, I learned how to manage a team, and take responsibility for my actions and decisions.</p>
-					<p>On my personal time, I enjoy horseback riding and playing the piano, and spend a lot of time doing sports. I am always ready to learn new skills and enjoy learning about everything.</p>
-				</div>
-			</div>
+          <div className='links'>
+            <a href='https://github.com/pmenage' rel="noopener" target="_blank">Github profile</a><br />
+            <a href='https://www.linkedin.com/in/pauline-m%C3%A9nage-030479109/' rel="noopener" target="_blank">LinkedIn profile</a><br />
+            <a href='https://medium.com/@pauline.menage26' rel="noopener" target="_blank">Medium blog posts</a>
+          </div>
+        </div>
+      </div>
 
-			<style jsx>{`
+      <div className='pure-u-sm-1 pure-u-md-1-2'>
+        <div className='about'>
+          <h2>About me</h2>
+          <p>Hello, I am Pauline, a software engineering student completing my fifth year at Bordeaux Graduate School of Engineering, in Computer Science. I am passionate about computer science, and I spend most of my free time coding or reading about new technologies.</p>
+          <p>As president of a Junior-Enterprise in 2016-2017, I learned how to manage a team, and take responsibility for my actions and decisions.</p>
+          <p>On my personal time, I enjoy horseback riding and playing the piano, and spend a lot of time doing sports. I am always ready to learn new skills and enjoy learning about everything.</p>
+        </div>
+      </div>
+
+      <style jsx>{`
         .pure-g {
           margin-bottom: 3em;
         }
@@ -139,7 +140,7 @@ export default () => (
         }
       `}</style>
 
-		</div>
+    </div>
 
-	</Layout>
+  </Layout>
 )
