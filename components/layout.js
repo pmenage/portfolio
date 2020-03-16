076f46922119528d6ce74d7b2f0e1babb32a0149
Pauline Menage <pauline.menage26@gmail.com>
2020-03-16 23:57:23 +0100

    Add Hotjar

--- components/layout.js
+++ components/layout.js
@@ -14,11 +14,23 @@ class Index extends React.Component {
 					<meta charSet='utf-8' />
 					<meta name='viewport' content='initial-scale=1.0, width=device-width' />
 					<meta name='description' content="Pauline's portfolio" />
+					<link rel="shortcut icon" href="/favicon.ico" />
 					<link href="https://fonts.googleapis.com/css?family=Roboto+Slab:300,400,500,600&display=swap" rel="stylesheet" />
 					<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&display=swap" rel="stylesheet" />
 					<link href="https://fonts.googleapis.com/css?family=Lato:200,300,400,500,600,700&display=swap" rel="stylesheet" />
 					<link rel="stylesheet" href="https://unpkg.com/purecss@0.6.2/build/pure-min.css" crossOrigin="anonymous" />
 					<link rel="stylesheet" href="https://unpkg.com/purecss@0.6.2/build/grids-responsive-min.css" crossOrigin="anonymous" />
+					<!-- Hotjar Tracking Code for https://www.paulinemenage.com -->
+					<script>
+						(function(h,o,t,j,a,r){
+							h.hj = h.hj || function () { (h.hj.q = h.hj.q || []).push(arguments) };
+							h._hjSettings={hjid:1731884,hjsv:6};
+							a=o.getElementsByTagName('head')[0];
+							r=o.createElement('script');r.async=1;
+							r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+							a.appendChild(r);
+						})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+					</script>
 					<style>{`
 						body {
 						background-color: #fff;
